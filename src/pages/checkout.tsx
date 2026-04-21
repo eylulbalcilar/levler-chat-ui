43ed389978c22d179998a3c940c26dfb9fc36ad6
hayfa safa <haifasafaa@gmail.com>
2026-04-21 23:33:34 +0200

    put the test in checkout page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -4,9 +4,22 @@ export default function Checklist() {
   return (
     <div>
       <h2>My Checklist</h2>
+      <p>3 tasks remaining</p>
 
       <div>
         <span>Workout</span>
+        <Button variant="neutral">Edit</Button>
+        <Button variant="subtle">Delete</Button>
+      </div>
+
+          <div>
+      <span>Clean floors</span>
+        <Button variant="neutral">Edit</Button>
+        <Button variant="subtle">Delete</Button>
+      </div>
+
+          <div>
+      <span>Cook dinner</span>
         <Button variant="neutral">Edit</Button>
         <Button variant="subtle">Delete</Button>
       </div>
